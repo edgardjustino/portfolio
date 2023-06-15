c1fd1e43bb7eeda292674bd1caec32df1114d594
edgardjustino <eddiejustino@gmail.com>
2023-06-15 02:52:56 -0300

    a brand new nav bar being responsive

---
+++ script.js
@@ -0,0 +1,14 @@
+const navMenu = document.getElementById('navmenu');
+const navToggle = document.getElementById('togglebtn');
+
+function toggle() {
+  navMenu.classList.toggle('active-toggle');
+  navToggle.classList.toggle('active-toggle');
+}
+
+document.onclick = function (e) {
+  if (e.target.id !== 'togglebtn' && e.target.id !== 'navmenu') {
+    navMenu.classList.remove('active-toggle');
+    navToggle.classList.remove('active-toggle');
+  }
+};
